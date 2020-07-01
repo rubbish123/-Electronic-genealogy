ff6329c13d138695d98c24165ea006cbcdaac66a
卿鸣秋 <1253880123@qq.com>
2020-07-01 11:37:18 +0800

    新建+删除族谱

---
+++ Project Z UI/页面/启动页面/登录页.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace Project_Z_UI
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)  //注册按钮
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)  //登录点击按钮
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)  //用户隐私手册
+        {
+
+        }
+    }
+}
