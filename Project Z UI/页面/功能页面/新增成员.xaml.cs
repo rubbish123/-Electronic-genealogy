29b9490c98f3802248c462e8d922bb6a7c81a387
rubbish123 <48993179+rubbish123@users.noreply.github.com>
2020-07-02 16:35:09 +0800

    Update 新增成员.xaml.cs

--- Project Z UI/页面/功能页面/新增成员.xaml.cs
+++ Project Z UI/页面/功能页面/新增成员.xaml.cs
@@ -31,6 +31,49 @@ namespace Project_Z_UI.页面.功能页面
         {
             IconHelper.RemoveIcon(this);
         }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string id = idTextbox.Text;
+            string name = nameTextbox.Text;
+            string password = passTextbox.Text;
+            string birth = birthTextbox.Text;
+            string fid = fidTextbox.Text;
+            string age = ageTextbox.Text;
+            string dai = daiTextbox.Text;
+            string admin = isAdminTextbox.Text;
+            string gender = genderTextbox.Text;
+            
+
+            string connStr = "Data Source=(local);Initial Catalog=family;User ID=sa;Password=qq123456";
+            string sql = string.Format("insert into t_user (编号,姓名,年龄,密码,代数,父亲编号,是否为管理员,性别,出生日期) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}')", id, name, age, password, dai, fid, admin,gender,birth);
+            //MessageBox.Show(sql);
+            //string sql = "insert into t_user (id,name,age,password,birth,dai,fid,admin) values ('2','张三','23','831143','2018-11-3','2','1','0')";
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            SqlCommand command = new SqlCommand(sql, conn);
+            //command.CommandType = CommandType.Text;
+            int s = command.ExecuteNonQuery();
+            if (s == 0)
+            {
+                MessageBox.Show("保存失败");
+            }
+            else
+            {
+                MessageBox.Show("保存成功");
+            }
+            conn.Close();
+
+            idTextbox.Text = "";
+            nameTextbox.Text = "";
+            ageTextbox.Text = "";
+            passTextbox.Text = "";
+            birthTextbox.Text = "";
+            daiTextbox.Text = "";
+            fidTextbox.Text = "";
+            isAdminTextbox.Text = "";
+            genderTextbox.Text = "";
+        }
     }
 
    
